d4d5582705ee89236b4202be0ec2073888791f89
m.bazrafkn <mhrn.bzrafkn.dev@gmail.com>
2023-04-03 15:46:57 -0700

    move matrix and jsondeepnetmodel classes to separate cs file completed.

---
+++ CSharpNet/JsonDeepNetModel.cs
@@ -0,0 +1,36 @@
+﻿using SimpleDeepNet.ExtensionMethods;
+using System.Collections.Generic;
+
+namespace CSharpNet;
+
+public class JsonDeepNetModel
+{
+    public double LearningRate { get; set; }
+    public int[] Layers { get; set; }
+    public List<double[]> HiddenValues { get; set; }
+    public List<double[][]> Weights { get; set; }
+
+    public JsonDeepNetModel()
+    {
+
+    }
+
+    public JsonDeepNetModel(DeepNetBuilder deepNet)
+    {
+        LearningRate = deepNet.LearningRate;
+        Layers = deepNet.Layers; HiddenValues = deepNet.HiddenValues;
+        Weights = deepNet.Weights.SerializeToJsonArray();
+    }
+
+    public static DeepNetBuilder Deserialize(JsonDeepNetModel model)
+    {
+        var deepNet = new DeepNetBuilder();
+
+        deepNet.LearningRate = model.LearningRate;
+        deepNet.Layers = model.Layers;
+        deepNet.HiddenValues = model.HiddenValues;
+        deepNet.Weights = model.Weights.DeserializeTo2DArray();
+
+        return deepNet;
+    }
+}
---
+++ CSharpNet/Matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpNet;
+
+public class Matrix
+{
+    public static double[,] Generate(int rows, int cols)
+    {
+        var matrix = new double[rows, cols];
+        var rnd = new Random();
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = (double)rnd.Next(-10000, 10000) / 10000;
+            }
+        }
+
+        return matrix;
+    }
+}
